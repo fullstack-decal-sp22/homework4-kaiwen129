25c3cff754c380ccdd86d06885c7a9fc1cdd04b3
Kaihao Wen <kaiwen129@berkeley.edu>
2022-03-22 13:50:33 -0700

    update

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,31 +1,46 @@
 import React from "react";
 import './styles/Board.css';
 import Square from "./Square";
+import { render } from "@testing-library/react";
 
 function Board() {
-
+    var currPlayer = 'X';
     const status = 'Next player: X';
 
-    function renderSquare(i) {
-        return <Square />;
+    function renderSquare(id, func, value) {
+        return <Square id={id} click_func={func} value={value}/>;
     }
+
+    function updateText(){
+      square1.setText('A');
+      /*Square.setHelper(currPlayer);
+      Square.setText(currPlayer);
+      if(currPlayer === 'X'){
+        currPlayer = 'O';
+      }
+      else{
+        currPlayer = 'X';
+      }*/
+    }
+
+    var square1 = renderSquare(1, updateText, 'X');
 
     return (  
         <div>
           <div className="board-row">
-            {renderSquare(0)}
-            {renderSquare(1)}
-            {renderSquare(2)}
+            {square1}
+            {renderSquare(1, updateText, 'O')}
+            {renderSquare(2, updateText, 'O')}
           </div>
           <div className="board-row">
-            {renderSquare(3)}
-            {renderSquare(4)}
-            {renderSquare(5)}
+            {renderSquare(3, updateText, 'O')}
+            {renderSquare(4, updateText, 'O')}
+            {renderSquare(5, updateText, 'O')}
           </div>
           <div className="board-row">
-            {renderSquare(6)}
-            {renderSquare(7)}
-            {renderSquare(8)}
+            {renderSquare(6, updateText, 'O')}
+            {renderSquare(7, updateText, 'O')}
+            {renderSquare(8, updateText, 'O')}
           </div>
           <div className="status">{status}</div>
         </div>
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import './styles/Square.css';
 
-function Square() {
+function Square(props) {
+    const id = props.id;
+    const updateText = props.click_func;
+    var value = props.value;
+
+    const [text, setText] = useState(null);
+
+    function setHelper(value){
+        setText(value);
+    };
+
     return (
-        <button className="square">
-            {/* TODO */}
+        <button className="square" onClick={updateText}>
+            {text}
         </button>
     )
+}
+
+function helper(player, func){
+    func(player);
 }
 
 export default Square;
